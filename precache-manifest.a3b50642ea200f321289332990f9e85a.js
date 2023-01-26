edd62d16e1d86264aad22107f50ca9544084b37c
Yuvraj <yg1941@srmist.edu.in>
2023-01-27 02:48:30 +0530

    Updates

---
+++ precache-manifest.a3b50642ea200f321289332990f9e85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d592a6db4e9921f5078",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/static/css/main.b3ce65e8.chunk.css"
+  },
+  {
+    "revision": "3d592a6db4e9921f5078",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/static/js/main.3d592a6d.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "2c6834e10c1bf2d7a2af",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/static/js/2.2c6834e1.chunk.js"
+  },
+  {
+    "revision": "d3f2e0394f68841cdb2d",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/static/js/runtime~main.d3f2e039.js"
+  },
+  {
+    "revision": "a32b48b5df844414f2dd3a3f74ac3725",
+    "url": "%7Bgithub-Yuvrajatgit%7D.github.io/%7Brepo-WebPageForm%7D/index.html"
+  }
+];
